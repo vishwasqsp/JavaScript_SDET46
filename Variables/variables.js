10c86778c615c2698a2e55bbdc084c70d7f7acef
vishwasqsp <vishwas.rpem17@gmail.com>
2023-03-15 23:09:31 +0530

    new

---
+++ Variables/variables.js
@@ -0,0 +1,14 @@
+// var a = 10
+// var a = 20
+// var a = 'javascript'
+// var a = 1+6
+
+// let a = 98
+// a =47
+// a= 'javascript'
+
+const a = true
+console.log(a)
+console.log(typeof(a))
+console.log(typeof(String(a)))
+console.log(typeof(Number(a)))
